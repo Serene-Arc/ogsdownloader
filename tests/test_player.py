8ff5a74a27486f16c1ac19584c8a01466e322496
Serene-Arc <serenical@gmail.com>
2022-02-19 15:08:24 +1000

    Fix broken test

--- tests/test_player.py
+++ tests/test_player.py
@@ -38,14 +38,14 @@ def test_get_user_data_from_id(test_user_id: int, expected: dict, config: Config
 
 
 @pytest.mark.parametrize(('test_user_id', 'expected_len_min'), (
-    (1070215, 4),
+    (1070215, 44),
 ))
 def test_get_games_from_user_id(test_user_id: int, expected_len_min: int, config: ConfigParser):
     test_user = Player(config, 5)
     test_user.load_tokens()
     result = test_user.get_games_from_user_id(test_user_id)
-    assert isinstance(result, dict)
-    assert result.get('count') <= expected_len_min
+    assert isinstance(result, list)
+    assert len(result) >= expected_len_min
 
 
 @pytest.mark.parametrize(('test_username', 'expected'), (
