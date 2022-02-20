d74bf5704db92568af478944ba1bb69dbc8c6d39
Serene-Arc <serenical@gmail.com>
2022-02-20 13:40:56 +1000

    Update test script

--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -4,6 +4,7 @@ if [[ -n "$TEST_PASSWORD" && -n "$TEST_USERNAME" ]]; then
     ( \
         echo "[DEFAULT]" && \
         echo "username = $TEST_USERNAME" && \
-        echo "password = $TEST_PASSWORD";
+        echo "password = $TEST_PASSWORD" && \
+        echo -e "client_id = bmATbhsh9RH7QXAR3n9Z88GBRoVqyxktj1RXcs9I\nclient_secret = eAeq0D4CB9akF9WNpYZmYk8ZfrtpqfTV5TuerNa006dR5BdcYrMxnBPAN4Y2m9B4NR3DIT0w3A1BqlJIOZXyIa8M32kIsg2q85HxsnWit5nYyHv9CeZafk5NLtww7iNq";
     ) >> ./test_config.cfg
 fi
